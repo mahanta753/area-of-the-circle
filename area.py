c04547c7c5a75b900d0d999c8f9b60b411dd4f15
mahanta753 <56730362+mahanta753@users.noreply.github.com>
2020-12-07 15:36:57 +0530

    Add files via upload

---
+++ area.py
@@ -0,0 +1,7 @@
+PI = 3.14
+#'PI' is a constant that has a value of 3.14
+r = float(input('Enter the radius of the circle:'))
+area=PI*(r**2)
+print("Area of the circle = %.3f"%area)
+#'%.3f' is used to display the output area which will have only three digits after the decimal point.
+
